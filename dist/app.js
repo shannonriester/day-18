00b3f646fd03cd9cc179114ac03b31a12f92e269
Shannon Riester <shannonriester@gmail.com>
2016-07-06 12:41:17 -0500

    first commit

---
+++ dist/app.js
@@ -0,0 +1,17 @@
+//<ol> should already be in html file as it loaded
+  //li elements can be stored in JSON object, then already include them in the HTML once it loads
+    //this way, the user can delete those <li>s if he/she feels like it
+      //this is the DELETE request!
+      //name-space li's with "data"
+
+console.log('hello');
+
+
+$.ajax({
+  // url:
+});
+
+
+//adding new item to <ol>
+  //this is the POST part!
+  //include data property(as JS element) in the ajax settings obj
